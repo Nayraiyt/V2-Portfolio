bd5b0127d171255d61023e19dc01866b4d108f42
Nayraiyt <naomimorganc@gmail.com>
2026-08-22 17:49:13 -0400

    Made more Mobile Friendly

--- js/index.js
+++ js/index.js
@@ -19,60 +19,47 @@ mm.add("(min-width: 600px)", () => {
     x: -700,
     duration: 3,
   });
-});
 
-mm.add("(max-width: 600px)", () => {
-  gsap.from(".nav-wrapper", {
+  gsap.from(".work-experience-box", {
     scrollTrigger: {
-      trigger: ".nav-wrapper",
-      start: "top",
+      trigger: ".work-experience-box",
+      start: "-40%",
       toggleActions: "play reverse play reverse",
     },
-    x: 800,
+    opacity: 0,
+    y: 50,
+    duration: 2,
+  });
+
+  gsap.from(".about-title-text", {
+    scrollTrigger: {
+      trigger: ".about-title-text",
+      start: "top 110%",
+      toggleActions: "play reverse play reverse",
+    },
+    y: 300,
+    duration: 1.5,
+  });
+
+  gsap.from(".path-title", {
+    scrollTrigger: {
+      trigger: ".path-title",
+      start: "top 100%",
+      toggleActions: "play reverse play reverse",
+    },
+    y: 100,
     duration: 1,
   });
-});
 
-gsap.from(".work-experience-box", {
-  scrollTrigger: {
-    trigger: ".work-experience-box",
-    start: "-40%",
-
-    toggleActions: "play reverse play reverse",
-  },
-  opacity: 0,
-  y: 50,
-  duration: 2,
-});
-
-gsap.from(".about-title-text", {
-  scrollTrigger: {
-    trigger: ".about-title-text",
-    start: "top 110%",
-    toggleActions: "play reverse play reverse",
-  },
-  y: 300,
-  duration: 1.5,
-});
-
-gsap.from(".path-title", {
-  scrollTrigger: {
-    trigger: ".path-title",
-    start: "top 100%",
-    toggleActions: "play reverse play reverse",
-  },
-  y: 100,
-  duration: 1,
-});
-
-gsap.from(".fancypaper-title", {
-  scrollTrigger: {
-    trigger: ".fancypaper-title",
-    start: "top 80%",
-    toggleActions: "play reverse play reverse",
-  },
-  x: -300,
-  duration: 2
+  gsap.from(".fancypaper-title", {
+    scrollTrigger: {
+      trigger: ".fancypaper-title",
+      start: "top 80%",
+      toggleActions: "play reverse play reverse",
+    },
+    x: -300,
+    duration: 2,
+  });
 });
 
 
